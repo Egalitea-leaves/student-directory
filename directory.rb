8b31dc0d98e54f3f134162ef5b2d2608e9c75c48
Egalitea-leaves <charlie.redwood@gmail.com>
2014-06-25 18:18:29 +0100

    Bare shit fixed bruv

--- directory.rb
+++ directory.rb
@@ -7,9 +7,9 @@ def print_header
 end
 
 def print(students)
-	students.sort_by do |cohort|
-		cohort[:cohort]
-	end	
+	# students.sort_by do |cohort|
+	# 	cohort[:cohort]
+	# end	
 	count = 0
   	while count < students.length do 
     	p "#{students[count][:name]}, (#{students[count][:cohort]} cohort), #{students[count][:hobby]}".center(@line_width) if students[count][:name].start_with?("C") && students[count][:name].length < 12
@@ -26,23 +26,30 @@ def input_students
 	# create an empty array
  	students = []
  	# get the first name
- 	name = gets.chomp.capitalize
+ 	name = gets.gsub(/\n/, "").capitalize 
  	while !name.empty? do
  		# while the name is not empty, repeat this code
-		p "Please enter the Month of the Cohort"
-		# get cohort
-		cohort = gets.chomp.capitalize.to_sym
-		cohort = :June if cohort.empty?
+		loop do
+			require 'date'
+			p "Please enter the Month of the Cohort"
+			# get cohort
+			@cohort = gets.gsub(/\n/, "").capitalize.to_sym
+			# cohort = :June if cohort.empty?
+		break if  Date::MONTHNAMES.include?(@cohort.to_s)
+	end
 		# set June as default		
 		p "Please enter the Hobby of the student"	
 		# get hobby
-		hobby = gets.chomp.capitalize.to_sym
+		hobby = gets.gsub(/\n/, "").capitalize.to_sym
 		# add new values to array
-		students << {:name => name, :cohort => cohort, :hobby => hobby}
-		p "Now we have #{students.length} students"	
+		students << {:name => name, :cohort => Date.parse(@cohort).strftime(format="%B").to_sym, :hobby => hobby}
+		if students.length == 1 
+			p "Now we have #{students.length} student" 
+			else p "Now we have #{students.length} students"
+		end	
 		p "Please enter the name of another student, or press return to finish"
 		# get another name
-		name = gets.chomp.capitalize
+		name = gets.gsub(/\n/, "").capitalize
 	end
 	students
 end	
